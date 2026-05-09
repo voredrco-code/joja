91555d35a1be982ef55d9d1b209344cfcf678705
voredrco-code <voredr.co@gmail.com>
2026-05-09 18:48:33 +0300

    Add missing ContactEmail migration to fix production 500 error

---
+++ Joja.Api/Migrations/20260509154708_AddContactEmailToSettings.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Joja.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddContactEmailToSettings : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ContactEmail",
+                table: "SiteSettings",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ContactEmail",
+                table: "SiteSettings");
+        }
+    }
+}
